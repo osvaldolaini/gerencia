857c1e602e41adf025a52e4bd0ac664ac7d3dd15
Osvaldo Laini <osvaldolaini@hotmail.com>
2025-03-24 20:19:58 -0300

    santos dumont v1.3.1 - App

--- app/Livewire/App/Dashboard.php
+++ app/Livewire/App/Dashboard.php
@@ -8,40 +8,40 @@ use Livewire\Component;
 
 class Dashboard extends Component
 {
-    // public $fo = false;
-    // public $fault = false;
-    // // Define o layout a ser usado
-    // protected $layout = 'app';
+    public $fo = false;
+    public $fault = false;
+    // Define o layout a ser usado
+    protected $layout = 'app';
 
-    // public $config;
-    // public $darkMode;
+    public $config;
+    public $darkMode;
 
-    // public function mount()
-    // {
-    //     $this->config = Settings::find(1);
-    //     $this->darkMode = Auth::user()->dark;
-    // }
+    public function mount()
+    {
+        $this->config = Settings::find(1);
+        $this->darkMode = Auth::user()->dark;
+    }
     public function render()
     {
         return view('livewire.app.dashboard')->layout('layouts.' . $this->layout);
     }
-    // public function fo_form()
-    // {
-    //     $this->fo = true;
-    // }
-    // public function fault_form()
-    // {
-    //     $this->fault = true;
-    // }
+    public function fo_form()
+    {
+        $this->fo = true;
+    }
+    public function fault_form()
+    {
+        $this->fault = true;
+    }
 
-    // //configs user
-    // public function toggleDarkMode()
-    // {
-    //     $user = Auth::user();
-    //     $user->dark = !$user->dark;
-    //     $user->save();
+    //configs user
+    public function toggleDarkMode()
+    {
+        $user = Auth::user();
+        $user->dark = !$user->dark;
+        $user->save();
 
-    //     $this->darkMode = $user->dark;
-    //     $this->dispatch('darkModeToggled', $this->darkMode);
-    // }
+        $this->darkMode = $user->dark;
+        $this->dispatch('darkModeToggled', $this->darkMode);
+    }
 }

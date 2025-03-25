47ba49de4fbc0251c470212e74d7d5d6b22d05d5
Osvaldo Laini <osvaldolaini@hotmail.com>
2025-03-24 22:29:06 -0300

    santos dumont v1.3.1 - App

--- resources/views/livewire/app/dashboard.blade.php
+++ resources/views/livewire/app/dashboard.blade.php
@@ -34,6 +34,9 @@
             <div id="tab3" x-show="activeTab === '#tab3'">
                 @livewire('app.user-profile')
             </div>
+            <div id="tab4" x-show="activeTab === '#tab3'">
+                @livewire('read-me')
+            </div>
         </x-slot>
         <x-slot name="nav">
             <footer
@@ -78,13 +81,18 @@
                             <path stroke-linecap="round" stroke-linejoin="round" d="M5 12h14M12 5l7 7-7 7">
                             </path>
                         </svg>
-                        <svg class="w-6 h-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                            stroke-width="1.5" stroke="currentColor" class="ml-2 size-6">
-                            <path stroke-linecap="round" stroke-linejoin="round"
-                                d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
-                        </svg>
                     </x-slot>
                     <x-slot name="title">Perfil</x-slot>
+                </x-app.tab-nav>
+                <x-app.tab-nav tab="tab4">
+                    <x-slot name="svg">
+                        <svg class="w-6 h-6" fill="none" stroke="currentColor" stroke-width="2" viewBox="0 0 24 24"
+                            xmlns="http://www.w3.org/2000/svg">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M5 12h14M12 5l7 7-7 7">
+                            </path>
+                        </svg>
+                    </x-slot>
+                    <x-slot name="title">Versão</x-slot>
                 </x-app.tab-nav>
 
             </footer>

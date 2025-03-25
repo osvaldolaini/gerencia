46a890f704147768745b14812f6d78d1c0724ab0
Osvaldo Laini <osvaldolaini@hotmail.com>
2025-03-24 23:51:58 -0300

    santos dumont v1.3.1 - App

--- resources/views/components/under-construction.blade.php
+++ resources/views/components/under-construction.blade.php
@@ -2,7 +2,7 @@
     <div class="max-w-md p-8 mx-auto mt-10 bg-white rounded-lg shadow-lg">
         <div class="flex justify-center mb-4 text-5xl text-yellow-500 animate-bounce">
 
-            <img class="w-10" src="{{ url('storage/logos/ovelha.png') }}" alt="api-gerencia">
+            <img class="w-10" src="{{ url('storage/svg/sheep.svg') }}" alt="api-gerencia">
         </div>
         <h1 class="flex justify-center mb-2 text-2xl font-bold text-gray-800">Página em Construção!</h1>
         <p class="flex justify-center text-gray-600">Estamos trabalhando duro para trazer algo incrível.

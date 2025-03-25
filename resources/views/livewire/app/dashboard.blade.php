f300b4f321d346d245a16b5c9b270e36f2d6fcf7
Osvaldo Laini <osvaldolaini@hotmail.com>
2025-03-24 22:37:43 -0300

    santos dumont v1.3.1 - App

--- resources/views/livewire/app/dashboard.blade.php
+++ resources/views/livewire/app/dashboard.blade.php
@@ -34,7 +34,7 @@
             <div id="tab3" x-show="activeTab === '#tab3'">
                 @livewire('app.user-profile')
             </div>
-            <div id="tab4" x-show="activeTab === '#tab3'">
+            <div id="tab4" x-show="activeTab === '#tab4'">
                 <div class="prose max-w-none dark:prose-invert">
                     {!! $readmeContent !!}
                 </div>
@@ -78,12 +78,10 @@
                 </x-app.tab-nav>
                 <x-app.tab-nav tab="tab3">
                     <x-slot name="svg">
-                        <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6" fill="none" viewBox="0 0 24 24"
-                            stroke-width="1.5" stroke="currentColor">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                            stroke="currentColor" class="w-6 h-6">
                             <path stroke-linecap="round" stroke-linejoin="round"
-                                d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Z" />
-                            <path stroke-linecap="round" stroke-linejoin="round"
-                                d="M19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10" />
+                                d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
                         </svg>
                     </x-slot>
                     <x-slot name="title">Perfil</x-slot>

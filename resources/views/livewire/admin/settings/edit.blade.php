b01341ae39a5dc3bc492d8f8065c92e4627fa42a
Osvaldo Laini <osvaldolaini@hotmail.com>
2025-03-29 20:51:49 -0300

    santos dumont v1.3.2 - App

--- resources/views/livewire/admin/settings/edit.blade.php
+++ resources/views/livewire/admin/settings/edit.blade.php
@@ -150,7 +150,8 @@
                                     </button>
                                 </div>
                             @elseif ($logo)
-                                <img src="{{ url('storage/' . $this->logo_path) }}" wire:model="logo_path">
+                                <img src="{{ url('storage/logos-school' . $this->logo_path) }}"
+                                    wire:model="logo_path">
                                 <div class="flex justify-end space-x-1">
                                     <button wire:click="excluirLogo()" type="button"
                                         class="text-red-600 inline-flex items-center hover:text-white border border-red-600 hover:bg-red-600 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:border-red-500 dark:text-red-500 dark:hover:text-white dark:hover:bg-red-600 dark:focus:ring-red-900">

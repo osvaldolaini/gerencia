2e182fabbb53f705177dff820a6290b1d38904c0
Osvaldo Laini <osvaldolaini@hotmail.com>
2025-03-29 21:10:41 -0300

    santos dumont v1.3.2 - App

--- app/Livewire/Admin/Settings/Settings.php
+++ app/Livewire/Admin/Settings/Settings.php
@@ -139,16 +139,15 @@ class Settings extends Component
             }
 
             if (isset($this->uploadimage)) {
-                $ext = $this->uploadimage->getClientOriginalExtension();
                 $code = Str::uuid();
                 $new_name = $code . '.png';
 
-                // $path = storage_path('app/public/logos-school');
+                $path = storage_path('app/public/logos-school');
 
-                // // Verifica se o diretório existe e, se não, cria com permissão 755
-                // if (!file_exists($path)) {
-                //     mkdir($path, 0755, true);
-                // }
+                // Verifica se o diretório existe e, se não, cria com permissão 755
+                if (!file_exists($path)) {
+                    mkdir($path, 0755, true);
+                }
 
                 $this->uploadimage->storeAs('public/logos-school', $new_name);
 

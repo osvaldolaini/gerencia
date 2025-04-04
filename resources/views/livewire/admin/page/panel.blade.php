24150e2a0b49b5d5c1a74e6219e1f4dbddef5eaa
Osvaldo Laini <osvaldolaini@hotmail.com>
2025-04-04 20:14:17 -0300

    santos dumont v1.3.5 - Painel

--- resources/views/livewire/admin/page/panel.blade.php
+++ resources/views/livewire/admin/page/panel.blade.php
@@ -2,7 +2,7 @@
     <div class="grid w-full grid-cols-6 gap-2">
         <span class="col-span-full sm:col-span-2">
             <div class="relative h-32 overflow-hidden bg-blue-500 rounded-lg shadow-md">
-                <svg class="absolute w-24 h-24 text-blue-800 rounded-md opacity-50 top-6 right-6 md:-right-4"
+                <svg class="absolute w-24 h-24 text-blue-800 rounded-md opacity-50 top-6 -right-6 md:-right-4"
                     viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                     <path
                         d="M13 20V18C13 15.2386 10.7614 13 8 13C5.23858 13 3 15.2386 3 18V20H13ZM13 20H21V19C21 16.0545 18.7614 14 16 14C14.5867 14 13.3103 14.6255 12.4009 15.6311M11 7C11 8.65685 9.65685 10 8 10C6.34315 10 5 8.65685 5 7C5 5.34315 6.34315 4 8 4C9.65685 4 11 5.34315 11 7ZM18 9C18 10.1046 17.1046 11 16 11C14.8954 11 14 10.1046 14 9C14 7.89543 14.8954 7 16 7C17.1046 7 18 7.89543 18 9Z"
@@ -30,11 +30,11 @@
                                     srcset="{{ url('storage/companies/' . $company->id . '/' . $company->code_image . '_list.png') }}" />
                                 <source
                                     srcset="{{ url('storage/companies/' . $company->id . '/' . $company->code_image . '_list.webp') }}" />
-                                <img class="absolute w-24 h-24 text-blue-800 rounded-md opacity-50 top-6 right-6 md:-right-4 src="{{ url('storage/companies/' . $company->id . '/' . $company->code_image . '_list.png') }}"
+                                <img class="absolute w-24 h-24 text-blue-800 rounded-md opacity-50 top-6 -right-6 md:-right-4 src="{{ url('storage/companies/' . $company->id . '/' . $company->code_image . '_list.png') }}"
                                     alt="{{ $company->name }}">
                             </picture>
                         @else
-                            <svg class="absolute w-24 h-24 text-blue-800 rounded-md opacity-50 top-6 right-6 md:-right-4"
+                            <svg class="absolute w-24 h-24 text-blue-800 rounded-md opacity-50 top-6 -right-6 md:-right-4"
                                 viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                                 <path
                                     d="M13 20V18C13 15.2386 10.7614 13 8 13C5.23858 13 3 15.2386 3 18V20H13ZM13 20H21V19C21 16.0545 18.7614 14 16 14C14.5867 14 13.3103 14.6255 12.4009 15.6311M11 7C11 8.65685 9.65685 10 8 10C6.34315 10 5 8.65685 5 7C5 5.34315 6.34315 4 8 4C9.65685 4 11 5.34315 11 7ZM18 9C18 10.1046 17.1046 11 16 11C14.8954 11 14 10.1046 14 9C14 7.89543 14.8954 7 16 7C17.1046 7 18 7.89543 18 9Z"
@@ -70,11 +70,11 @@
                                                 srcset="{{ url('storage/schoolGrades/' . $grade->id . '/' . $grade->code_image . '_list.png') }}" />
                                             <source
                                                 srcset="{{ url('storage/schoolGrades/' . $grade->id . '/' . $grade->code_image . '_list.webp') }}" />
-                                            <img class="absolute w-24 h-24 text-red-800 rounded-md opacity-50 top-6 right-6 md:-right-4 src="{{ url('storage/companies/' . $grade->id . '/' . $grade->code_image . '_list.png') }}"
+                                            <img class="absolute w-24 h-24 text-red-800 rounded-md opacity-50 top-6 -right-6 md:-right-4 src="{{ url('storage/companies/' . $grade->id . '/' . $grade->code_image . '_list.png') }}"
                                                 alt="{{ $grade->name }}">
                                         </picture>
                                     @else
-                                        <svg class="absolute w-24 h-24 text-red-800 rounded-md opacity-50 top-6 right-6 md:-right-4"
+                                        <svg class="absolute w-24 h-24 text-red-800 rounded-md opacity-50 top-6 -right-6 md:-right-4"
                                             viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                                             <path
                                                 d="M13 20V18C13 15.2386 10.7614 13 8 13C5.23858 13 3 15.2386 3 18V20H13ZM13 20H21V19C21 16.0545 18.7614 14 16 14C14.5867 14 13.3103 14.6255 12.4009 15.6311M11 7C11 8.65685 9.65685 10 8 10C6.34315 10 5 8.65685 5 7C5 5.34315 6.34315 4 8 4C9.65685 4 11 5.34315 11 7ZM18 9C18 10.1046 17.1046 11 16 11C14.8954 11 14 10.1046 14 9C14 7.89543 14.8954 7 16 7C17.1046 7 18 7.89543 18 9Z"

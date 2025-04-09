786f2eaacbdc3782e005ed24e8ba4da51fd85e12
Osvaldo Laini <osvaldolaini@hotmail.com>
2025-04-09 17:09:48 -0300

    santos dumont v1.3.7

--- resources/views/livewire/app/faults/fault-insert-simple.blade.php
+++ resources/views/livewire/app/faults/fault-insert-simple.blade.php
@@ -10,7 +10,7 @@
                     <label class="block text-sm font-medium text-gray-900 dark:text-white" for="qtd" required>
                         Períodos (qtd)</label>
                     <input type="number" wire:model="qtd" required
-                        class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500">
+                        class="bg-gray-50 text-base border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500">
                     @error('qtd')
                         <span class="error">{{ $message }}</span>
                     @enderror

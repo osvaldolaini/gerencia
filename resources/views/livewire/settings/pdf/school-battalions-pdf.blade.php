6fb86fb712a1bb9c89258c36474123a37f7407a4
Osvaldo Laini <osvaldolaini@hotmail.com>
2025-04-09 15:43:28 -0300

    santos dumont v1.3.7

--- resources/views/livewire/settings/pdf/school-battalions-pdf.blade.php
+++ resources/views/livewire/settings/pdf/school-battalions-pdf.blade.php
@@ -32,8 +32,8 @@
             width: 33.33%;
             /* Garante três colunas iguais */
             padding: 16px;
-            border: 1px solid #ddd;
-            border-radius: 8px;
+            /* border: 1px solid #ddd;
+            border-radius: 8px; */
             text-align: center;
             vertical-align: top;
         }
@@ -72,76 +72,6 @@
 
         .student-item p {
             font-size: 14px;
-        }
-
-        /* Estilos para mPDF */
-        @media print {
-            .container {
-                width: 100%;
-                font-family: sans-serif;
-                margin-top: 40px;
-            }
-
-            .grade-title {
-                display: block;
-                width: 100%;
-                text-align: center;
-                font-size: 50px !important;
-                font-weight: bold;
-                color: #555;
-                margin-bottom: 20px;
-            }
-
-            .container table {
-                width: 100%;
-                border-collapse: collapse;
-            }
-
-            .container td {
-                width: 33.33%;
-                padding: 16px;
-                /* border: 1px solid #ddd;
-                border-radius: 8px; */
-                text-align: center;
-                vertical-align: top;
-            }
-
-            .posto-container h2 {
-                font-size: 18px !important;
-                font-weight: bold;
-                margin-bottom: 10px;
-                width: 100%;
-            }
-
-            .rank-image {
-                width: 3rem;
-                height: auto;
-                border-radius: 50%;
-                display: block;
-                margin: 0 auto 10px;
-            }
-
-            .student-item {
-                padding-top: 8px;
-                padding-bottom: 8px;
-                border-bottom: 1px solid #eee;
-            }
-
-            .student-image {
-                width: 5rem;
-                height: auto;
-                margin: 0 auto 8px;
-                border-radius: 50%;
-            }
-
-            .student-item h3 {
-                font-weight: 600;
-                margin-bottom: 5px;
-            }
-
-            .student-item p {
-                font-size: 14px;
-            }
         }
     </style>
 </head>

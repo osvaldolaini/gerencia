fa366d509c1e073ad01a1b6b85a09888777541be
Osvaldo Laini <osvaldolaini@hotmail.com>
2025-04-16 15:15:52 -0300

    santos dumont v1.3.10

--- resources/views/livewire/discipline/fault-disciplines/fault-discipline-edit.blade.php
+++ resources/views/livewire/discipline/fault-disciplines/fault-discipline-edit.blade.php
@@ -148,7 +148,17 @@
                             </div>
                             <div class="col-span-full sm:col-span-4">
                                 <div class="grid grid-cols-2 gap-2 mb-1 sm:grid-cols-3">
-                                    <div class="col-span-full sm:col-span-3">
+                                    <div class="col-span-full sm:col-span-2">
+                                        <label class="block text-sm font-medium text-gray-900 dark:text-white"
+                                            for="title">
+                                            Nome completo</label>
+                                        <input type="text" wire:model="al_name" readonly
+                                            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500">
+                                        @error('al_name')
+                                            <span class="error">{{ $message }}</span>
+                                        @enderror
+                                    </div>
+                                    <div class="col-span-full sm:col-span-1">
                                         <label class="block text-sm font-medium text-gray-900 dark:text-white"
                                             for="title">
                                             Nome aluno</label>

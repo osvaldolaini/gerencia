57e0b8223f6fce3002d622ee848165d65f6834a8
Osvaldo Laini <osvaldolaini@hotmail.com>
2025-04-25 16:39:33 -0300

    Versão Santos Dumont 1.3.11

--- resources/views/livewire/students/student-list.blade.php
+++ resources/views/livewire/students/student-list.blade.php
@@ -123,7 +123,7 @@
                                     </div>
                                     <div wire:ignore>
                                         <div class="p-0 tooltip tooltip-top" data-tip="Ficha">
-                                            <button wire:click="history($item->id)"
+                                            <button wire:click="history({{ $item->id }})"
                                                 class="px-3 py-2 transition-colors duration-200 rounded-sm hover:text-white dark:hover:bg-blue-500 hover:bg-blue-500 whitespace-nowrap">
                                                 <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6"
                                                     fill="currentColor" viewBox="0 0 512 512" xml:space="preserve">

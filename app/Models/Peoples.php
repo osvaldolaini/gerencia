1b4c1d8ddb0e2624dee5c5aa7a8136dd45e752fc
Osvaldo Laini <osvaldolaini@hotmail.com>
2025-05-06 15:35:33 -0300

    Versão Santos Dumont 1.3.14

--- app/Models/Peoples.php
+++ app/Models/Peoples.php
@@ -175,10 +175,11 @@ class Peoples extends Model
     // }
     public function getAdjustedGrauAttribute()
     {
+        // $nota = number_format(floatval($this->grau), 2);
         $nota = number_format(floatval($this->grau), 2);
         $punicoes = $this->fafd()->whereNotNull('bi_date')->orderBy('bi_date')->get();
         $dataReferencia = null;
-
+        Log::debug("Nota inicial sem float: {$this->grau}");
         Log::debug("Nota inicial: {$nota}");
 
         if ($punicoes->isEmpty()) {

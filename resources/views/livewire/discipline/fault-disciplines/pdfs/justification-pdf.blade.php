fbe09d3747be9bce67d149280f78255f5323a956
Osvaldo Laini <osvaldolaini@hotmail.com>
2025-05-14 15:15:54 -0300

    Versão Santos Dumont 1.4.2

--- resources/views/livewire/discipline/fault-disciplines/pdfs/justification-pdf.blade.php
+++ resources/views/livewire/discipline/fault-disciplines/pdfs/justification-pdf.blade.php
@@ -215,7 +215,7 @@
                         width: 100%;
                         text-align: center;
                         padding-top: 30px;">
-                        {{ mb_strtoupper($fault_discipline->company?->cmt_cia_posto ?? MilitaryRank::fromDb($fault_discipline?->company?->comandant?->posto_grad)?->label()) }}
+                        {{ mb_strtoupper($fault_discipline->company?->cmt_cia_posto ?? $fault_discipline?->company?->comandant?->posto_grad) }}
                         {{ mb_strtoupper($fault_discipline->company?->cmt_cia ?? $fault_discipline?->company?->comandant?->posto_grad) }}
                         <p>
                             COMANDANTE DA {{ mb_strtoupper($fault_discipline->cia) }}

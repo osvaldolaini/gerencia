ed5357213e0d6aa98e79a6fbbb84da511e8a58c6
Osvaldo Laini <osvaldolaini@hotmail.com>
2025-05-15 16:34:30 -0300

    Versão Santos Dumont 1.4.2

--- resources/views/livewire/settings/pdf/student-history-pdf.blade.php
+++ resources/views/livewire/settings/pdf/student-history-pdf.blade.php
@@ -97,7 +97,8 @@
                         <div class="linha-info"><strong>Nome aluno:</strong> {{ $student->nick }}</div>
                         <div class="linha-info"><strong>Nr:</strong> {{ $student->number }}</div>
                         <div class="linha-info"><strong>Turma:</strong> {{ $student->people_class }}</div>
-                        <div class="linha-info"><strong>Grau de comportamento:</strong> {{ $student->adjusted_grau }}
+                        <div class="linha-info"><strong>Grau de comportamento: {{ $student->grau_status }}</strong>
+                            ({{ $student->adjusted_grau }})
                         </div>
                     </td>
 

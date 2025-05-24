e94acab55fbe30c1c87a2ab1a66521b6c1f1b24e
Osvaldo Laini <osvaldolaini@hotmail.com>
2025-05-24 09:33:18 -0300

    Versão Santos Dumont 1.4.4

--- app/Livewire/Discipline/FaultDisciplines/FaultDisciplineJustification.php
+++ app/Livewire/Discipline/FaultDisciplines/FaultDisciplineJustification.php
@@ -12,13 +12,15 @@ use Livewire\WithFileUploads;
 use App\Models\Admin\Settings\Settings;
 use App\Models\Discipline\Settings\Faults;
 use App\Models\Settings\Companies;
+use App\Traits\HandlesPdfUploads;
 use Illuminate\Support\Facades\Auth;
 
 use Illuminate\Support\Str;
 
 class FaultDisciplineJustification extends Component
 {
-    use WithFileUploads;
+    use WithFileUploads;      // ⬅️ Necessário para lidar com uploads
+    use HandlesPdfUploads;    // ⬅️ Sua trait personalizada
 
     public $uploadPdf;
     public $fafd;

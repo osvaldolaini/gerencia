5a3bec48bccfc1ea0a8a5a5c45e9054a492c066c
Osvaldo Laini <osvaldolaini@hotmail.com>
2025-06-13 21:33:45 -0300

    Versão Santos Dumont 1.5.7

--- resources/views/livewire/discipline/panel/discipline-panel-card.blade.php
+++ resources/views/livewire/discipline/panel/discipline-panel-card.blade.php
@@ -41,7 +41,7 @@
                         <span class="flex flex-col items-center justify-center text-center">
                             Atrasado
                             <span class="flex text-center badge badge-error">
-                                {{ $fafd->where('delivered_date', '>', date('Y-d-m'))->count() }}
+                                {{ $fafd->where('justification_date', '<', date('Y-d-m'))->count() }}
                             </span>
                         </span>
                     </div>

70960dd2e3b104170c1c65b72777dcd447e502fe
Osvaldo Laini <osvaldolaini@hotmail.com>
2025-06-13 21:09:16 -0300

    Versão Santos Dumont 1.5.7

--- app/Livewire/Settings/SchoolClassroomSeats/SchoolClassroomSeatForm.php
+++ app/Livewire/Settings/SchoolClassroomSeats/SchoolClassroomSeatForm.php
@@ -10,6 +10,13 @@ use Livewire\Component;
 use App\Models\Peoples;
 use App\Models\Settings\SchoolClassesYears;
 
+
+use App\Models\Admin\Settings\Settings;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Storage;
+
+use Illuminate\Support\Str;
+
 class SchoolClassroomSeatForm extends Component
 {
     public $breadcrumb = 'Turma: ';
@@ -60,20 +67,9 @@ class SchoolClassroomSeatForm extends Component
     public function render()
     {
         if ($this->inputSearch != '') {
-            // $this->results = Peoples::select('id', 'name', 'number', 'nick', 'sex', 'logo_path')
-            //     ->where('nick', 'LIKE', '%' . $this->inputSearch . '%')
-            //     ->where('type', 1)
-            //     ->orderBy('nick', 'asc')
-            //     ->limit(5)
-            //     ->get();
-
             if ($this->field) {
                 $pluckStudents = [];
-                // $classes = SchoolClasses::where('school_grade_id', $this->field)->where('active', 1)->get();
-                // foreach ($classes as $class) {
-                //     $pluckStudents = array_merge($pluckStudents, $class->studentsPivot->pluck('people_id')->toArray());
-                // }
-                // dd($pluckStudents);
+
                 $pluckStudents = $this->school_classes->studentsPivot->pluck('people_id')->toArray();
                 $this->results = Peoples::select('id', 'name', 'number', 'nick', 'sex', 'logo_path')
                     ->where('nick', 'LIKE', '%' . $this->inputSearch . '%')
@@ -141,5 +137,87 @@ class SchoolClassroomSeatForm extends Component
     public function openAlert($status, $msg)
     {
         $this->dispatch('openAlert', $status, $msg);
+    }
+
+    //PDF
+    public function classroom()
+    {
+
+        //Apagar itens do diretório temporário
+        $this->clearTmpDirectory('public/pdf-tmp');
+
+        // dd($this->school_classes);
+        $config = Settings::find(1);
+
+        $logoPath = Storage::exists('public/companies/' . $this->company->id)
+            ? url('storage/companies/' . $this->company->id . '/' . $this->company->code_image . '_list.png')
+            : url('storage/logos-school/logo-header.png');
+
+        // $seats = ClassroomSeats::with('students')
+        //     ->where('school_classes_id', $this->school_classes_id)
+        //     ->get();
+        // Crie uma instância do mPDF
+        $mpdf = new \Mpdf\Mpdf([
+            'mode'          => 'utf-8',
+            // 'orientation'        => 'P', //[P,L]
+            'format' => 'A4-P',
+            'margin_left'   => 15,
+            'margin_top'    => 15,
+            'default_font_size'  => 9,
+            'default_font'  => 'arial',
+        ]);
+        // dd($this->school_classes->first()->seats);
+        // dd($mpdf);
+        $html = view(
+            'livewire.settings.pdf.school-classrom-seats-pdf',
+            [
+                'logoPath'          => $logoPath,
+                'school_classes'    => $this->school_classes,
+                // 'seats'             => $seats,
+                'grade'             => $this->grade->name,
+                'config'            => $config,
+                'companies'         => $this->company,
+                'subtext'           => 'Turmas do ' . $this->grade->name,
+                'responsible'       => Auth::user()->name,
+            ]
+        )->render();
+
+        // Adicione o conteúdo HTML ao PDF
+        $mpdf->SetHTMLHeader('
+              <table width="100%">
+                  <tr >
+                      <td width="50%">
+                          <img width="50" src="' . $logoPath . '" alt="Logo">
+                      </td>
+                      <td width="50%" style="text-align: right;">
+                          <strong>' . $config->name . '</strong><br>
+                          ' . $this->company->name . '<br>
+                          Turmas do ' . $this->grade->name . '
+                      </td>
+                  </tr>
+              </table>
+              ');
+        $mpdf->SetHTMLFooter('
+       <table width="100%">
+           <tr>
+               <td width="66%">Impressão realizada em {DATE j/m/Y} às {DATE H:i:s}</td>
+               <td width="33%" style="text-align: right;">{PAGENO}/{nbpg}</td>
+           </tr>
+       </table>');
+        $mpdf->WriteHTML($html);
+
+        // Salve o PDF temporariamente
+        $file = trim('chamada_' . $this->grade->name . '_' . Str::uuid() . '.pdf');
+
+        if (!is_dir(storage_path('app/public/pdf-tmp'))) {
+            mkdir(storage_path('app/public/pdf-tmp'), 0775, true); // Cria o diretório, incluindo os subdiretórios, se necessário
+        }
+
+        $down = storage_path('app/public/pdf-tmp/' . $file);
+        $pdfPath = url('storage/pdf-tmp/' . $file);
+
+        $mpdf->Output($down, 'F');
+
+        $this->dispatch('openPdfInNewTabClasses', pdfPath: $pdfPath);
     }
 }

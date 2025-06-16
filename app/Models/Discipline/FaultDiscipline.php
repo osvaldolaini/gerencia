2fbed664b811b04a4a97be90df323f3be00e7f7c
Osvaldo Laini <osvaldolaini@hotmail.com>
2025-06-16 15:58:10 -0300

    Versão Santos Dumont 1.5.7

--- app/Models/Discipline/FaultDiscipline.php
+++ app/Models/Discipline/FaultDiscipline.php
@@ -84,6 +84,7 @@ class FaultDiscipline extends Model
         static::creating(function ($transaction) {
             $transaction->created_by = Auth::user()->name;
             $transaction->updated_by = Auth::user()->name;
+            $transaction->fafd = 0;
 
             // Obtém o ano atual
             $anoAtual = now()->year;
--- resources/views/livewire/discipline/fact-observeds/fact-observed-list.blade.php
+++ resources/views/livewire/discipline/fact-observeds/fact-observed-list.blade.php
@@ -65,8 +65,8 @@
                 <label
                     class="flex flex-col mx-auto justify-center px-3 py-2 transition-colors duration-200
                                 rounded-md cursor-pointer {{ $fafd ? 'bg-blue-500 text-gray-800' : 'bg-gray-800 text-white dark:bg-gray-100 dark:text-gray-900' }}">
-                    <input type="radio" wire:model.live="fafd" value="1" class="hidden peer"
-                        {{ $fafd ? 'checked' : '' }}>
+                    <input type="radio" wire:click='btnFafd()' wire:model.live="fafd" value="1"
+                        class="hidden peer" {{ $fafd ? 'checked' : '' }}>
                     <span class="text-xs">
                         Gerou FAFD?
                     </span>

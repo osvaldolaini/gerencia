e090a0259d70e4767382ad8f02d7c6a8b16a4a57
Osvaldo Laini <osvaldolaini@hotmail.com>
2025-06-23 16:05:23 -0300

    Versão Santos Dumont 1.5.8

--- resources/views/livewire/discipline/fact-observeds/fact-observed-form.blade.php
+++ resources/views/livewire/discipline/fact-observeds/fact-observed-form.blade.php
@@ -37,7 +37,7 @@
                             <div class="col-span-full sm:col-span-1 ">
                                 <label class="block text-sm font-medium text-gray-900 dark:text-white" for="title">
                                     Tipo do FO</label>
-                                <select wire:model="fact_type"
+                                <select wire:model.live="fact_type"
                                     class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500">
                                     <option value="">Selecione...</option>
                                     <option value="negativo">FO-</option>

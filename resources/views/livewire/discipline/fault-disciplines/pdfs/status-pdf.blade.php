07251102387222bfd1579cb85c82917fa3a456ef
Osvaldo Laini <osvaldolaini@hotmail.com>
2025-06-24 17:08:25 -0300

    Versão Santos Dumont 1.5.9

--- resources/views/livewire/discipline/fault-disciplines/pdfs/status-pdf.blade.php
+++ resources/views/livewire/discipline/fault-disciplines/pdfs/status-pdf.blade.php
@@ -168,7 +168,7 @@
                         <td class="text-left border">{{ $fafd->number }}/{{ $fafd->year }}</td>
                         <td class="text-left border">{{ $fafd->active == 1 ? 'Ativa' : 'Excluida' }}</td>
                         <td class="text-left border">{{ $fafd?->students?->nick ?? $fafd->al_nick }}
-                            ({{ $fafd?->students?->number }})
+                            ({{ $fafd?->students?->number ?? $fafd->al_number }})
                         </td>
                         <td class="text-left border">{{ $fafd?->students?->al_class->title }}</td>
                         <td class="text-left border">{{ $fafd?->f_date }}</td>

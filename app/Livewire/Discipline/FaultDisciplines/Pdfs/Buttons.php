54685af78afcee14924dd5efd0f4b2ee61f1c161
Osvaldo Laini <osvaldolaini@hotmail.com>
2025-06-24 16:50:46 -0300

    Versão Santos Dumont 1.5.9

--- app/Livewire/Discipline/FaultDisciplines/Pdfs/Buttons.php
+++ app/Livewire/Discipline/FaultDisciplines/Pdfs/Buttons.php
@@ -29,73 +29,73 @@ class Buttons extends Component
         return view('livewire.discipline.fault-disciplines.pdfs.buttons');
     }
     //Todas
-    // public function all()
-    // {
-    //     //Apagar itens do diretório temporário
-    //     $this->clearTmpDirectory('public/pdf-tmp');
+    public function all()
+    {
+        //Apagar itens do diretório temporário
+        $this->clearTmpDirectory('public/pdf-tmp');
 
-    //     $config = Settings::find(1);
-    //     $logoPath = url('storage/logos-school/logo-header.png');
+        $config = Settings::find(1);
+        $logoPath = url('storage/logos-school/logo-header.png');
 
 
-    //     // Crie uma instância do mPDF
-    //     $mpdf = new \Mpdf\Mpdf([
-    //         'mode'          => 'utf-8',
-    //         // 'orientation'        => 'P', //[P,L]
-    //         'format' => 'A4-P',
-    //         'margin_left'   => 15,
-    //         'margin_top'    => 15,
-    //         'default_font_size'  => 9,
-    //         'default_font'  => 'arial',
-    //     ]);
-    //     // dd($mpdf);
-    //     $html = view(
-    //         'livewire.discipline.fault-disciplines.pdfs.status-pdf',
-    //         [
-    //             'logoPath'          => $logoPath,
-    //             'title'             => 'Todas',
-    //             'data'              => FaultDiscipline::get(),
-    //             'config'            => $config,
-    //             'responsible'       => Auth::user()->name,
-    //         ]
-    //     )->render();
+        // Crie uma instância do mPDF
+        $mpdf = new \Mpdf\Mpdf([
+            'mode'          => 'utf-8',
+            // 'orientation'        => 'P', //[P,L]
+            'format' => 'A4-P',
+            'margin_left'   => 15,
+            'margin_top'    => 15,
+            'default_font_size'  => 9,
+            'default_font'  => 'arial',
+        ]);
+        // dd($mpdf);
+        $html = view(
+            'livewire.discipline.fault-disciplines.pdfs.status-pdf',
+            [
+                'logoPath'          => $logoPath,
+                'title'             => 'Todas',
+                'data'              => FaultDiscipline::where('active', 1)->where('justification_date', NULL)->get(),
+                'config'            => $config,
+                'responsible'       => Auth::user()->name,
+            ]
+        )->render();
 
-    //     // Adicione o conteúdo HTML ao PDF
-    //     $mpdf->SetHTMLHeader('
-    //          <table width="100%">
-    //              <tr >
-    //                  <td width="50%">
-    //                      <img width="50" src="' . $logoPath . '" alt="Logo">
-    //                  </td>
-    //                  <td width="50%" style="text-align: right;">
-    //                      <strong>' . $config->name . '</strong><br>
+        // Adicione o conteúdo HTML ao PDF
+        $mpdf->SetHTMLHeader('
+             <table width="100%">
+                 <tr >
+                     <td width="50%">
+                         <img width="50" src="' . $logoPath . '" alt="Logo">
+                     </td>
+                     <td width="50%" style="text-align: right;">
+                         <strong>' . $config->name . '</strong><br>
 
-    //                  </td>
-    //              </tr>
-    //          </table>
-    //          ');
-    //     $mpdf->SetHTMLFooter('
-    //   <table width="100%">
-    //       <tr>
-    //           <td width="66%">Impressão realizada em {DATE j/m/Y} às {DATE H:i:s}</td>
-    //           <td width="33%" style="text-align: right;">{PAGENO}/{nbpg}</td>
-    //       </tr>
-    //   </table>');
-    //     $mpdf->WriteHTML($html);
+                     </td>
+                 </tr>
+             </table>
+             ');
+        $mpdf->SetHTMLFooter('
+      <table width="100%">
+          <tr>
+              <td width="66%">Impressão realizada em {DATE j/m/Y} às {DATE H:i:s}</td>
+              <td width="33%" style="text-align: right;">{PAGENO}/{nbpg}</td>
+          </tr>
+      </table>');
+        $mpdf->WriteHTML($html);
 
-    //     // Salve o PDF temporariamente
-    //     $file = trim('todas_' . Str::uuid() . '.pdf');
+        // Salve o PDF temporariamente
+        $file = trim('todas_' . Str::uuid() . '.pdf');
 
-    //     if (!is_dir(storage_path('app/public/pdf-tmp'))) {
-    //         mkdir(storage_path('app/public/pdf-tmp'), 0775, true); // Cria o diretório, incluindo os subdiretórios, se necessário
-    //     }
+        if (!is_dir(storage_path('app/public/pdf-tmp'))) {
+            mkdir(storage_path('app/public/pdf-tmp'), 0775, true); // Cria o diretório, incluindo os subdiretórios, se necessário
+        }
 
-    //     $down = storage_path('app/public/pdf-tmp/' . $file);
-    //     $pdfPath = url('storage/pdf-tmp/' . $file);
+        $down = storage_path('app/public/pdf-tmp/' . $file);
+        $pdfPath = url('storage/pdf-tmp/' . $file);
 
-    //     $mpdf->Output($down, 'F');
-    //     $this->dispatch('openPdfInNewTabClasses', pdfPath: $pdfPath);
-    // }
+        $mpdf->Output($down, 'F');
+        $this->dispatch('openPdfInNewTabClasses', pdfPath: $pdfPath);
+    }
     //Justificativa
     public function justify()
     {

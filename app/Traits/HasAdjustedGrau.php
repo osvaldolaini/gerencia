dcd051ecc2b1f5626463f313391f7d0f052f0ce0
Osvaldo Laini <osvaldolaini@hotmail.com>
2025-07-01 09:59:28 -0300

    Versão Santos Dumont 1.7.0

--- app/Traits/HasAdjustedGrau.php
+++ app/Traits/HasAdjustedGrau.php
@@ -73,6 +73,7 @@ trait HasAdjustedGrau
             ->orderBy('bi_date')
             ->get();
 
+        Log::debug("Nota inicial: {$elogios}");
 
         $dataReferencia = null;
         // Log::debug("Sem punições. Dias após 90 da matrícula: {$dias}. Aumento: {$incremento}. Nota final: {$nota}");

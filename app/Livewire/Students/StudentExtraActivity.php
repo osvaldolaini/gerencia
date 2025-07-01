b6a83ccb0a7d0b9270656107f8186fd0bc5d1c47
Osvaldo Laini <osvaldolaini@hotmail.com>
2025-07-01 19:15:22 -0300

    Versão Santos Dumont 1.7.0

--- app/Livewire/Students/StudentExtraActivity.php
+++ app/Livewire/Students/StudentExtraActivity.php
@@ -2,21 +2,27 @@
 
 namespace App\Livewire\Students;
 
+use App\Models\Extracurricular\ExtraActivities;
 use App\Models\Extracurricular\StudentActivities;
 use App\Models\Peoples;
 use Livewire\Component;
 
 class StudentExtraActivity extends Component
 {
-    public $activities;
+    public $studentActivities;
     public $student;
     public $id;
 
     public $showJetModal = false;
+    public $showModalForm = false;
+
+    public $activities;
+    public $gip = 0;
     public function mount(Peoples $student)
     {
+        $this->activities = ExtraActivities::where("active", 1)->orderBy('title', 'asc')->get();
         $this->student  =  $student;
-        $this->activities   = $this->student->activities->where('active', 1)->sortByDesc('title');
+        $this->studentActivities   = $this->student->activities->where('active', 1)->sortByDesc('title');
     }
 
     public function render()
@@ -43,6 +49,24 @@ class StudentExtraActivity extends Component
         $this->openAlert('success', 'Registro excluido com sucesso.');
 
         $this->showJetModal = false;
+    }
+
+    //CREATE
+    public function showNewActivity()
+    {
+        $this->showModalForm = true;
+    }
+    public function addActivity()
+    {
+        StudentActivities::updateOrCreate([
+            'student_id'      => $this->students->id,
+            'extra_activities_id' => $this->extra_activities_id,
+        ], [
+            'active'         => 1,
+            'gip'            => $this->gip,
+        ]);
+        $this->showJetModal = true;
+        $this->openAlert('success', 'Inclusão na atividade realizada com sucesso');
     }
     //MESSAGE
     public function openAlert($status, $msg)
--- resources/views/livewire/students/student-extra-activity.blade.php
+++ resources/views/livewire/students/student-extra-activity.blade.php
@@ -3,8 +3,8 @@
         {{-- @livewire('message-alert-modal') --}}
         <span wire:click="showNewActivity()"
             class="flex items-center justify-between px-3 py-1 text-white transition-colors duration-200 bg-green-500 border border-gray-500 rounded-md cursor-pointer hover:text-white dark:hover:bg-blue-500 hover:hover:bg-blue-500 whitespace-nowrap">
-            Inserir atividade extra
             <x-layout.svg.plus class="w-4 h-4 mr-0 lg:mr-2"></x-layout.svg.plus>
+            Inserir atividade extra
         </span>
     </div>
     <div class="overflow-x-auto dark:text-white">
@@ -12,7 +12,7 @@
             <!-- head -->
             <thead>
                 <tr class="dark:text-white">
-                    <th>Atividade</th>
+                    <th>ATIVIDADE</th>
                     <th>GIP</th>
                     <th>PONTO EXTRA</th>
                     <th>APAGAR</th>
@@ -20,7 +20,7 @@
                 </tr>
             </thead>
             <tbody>
-                @foreach ($activities as $extra)
+                @foreach ($studentActivities as $extra)
                     <tr>
                         <td>
                             {{ $extra?->activity?->title }}
@@ -73,4 +73,56 @@
             </x-danger-button>
         </x-slot>
     </x-confirmation-modal>
+    <x-dialog-modal wire:model="showModalForm" maxWidth="4xl">
+        <x-slot name="title">{{ $breadcrumb }} </x-slot>
+        <x-slot name="content">
+            <form>
+                <div role="tabpanel"
+                    class="p-6 border-2 rounded-r-lg rounded-bl-lg bg-base-100 border-base-300 dark:bg-gray-700 dark:text-gray-100">
+                    <div class="grid grid-cols-4 gap-2 mb-1 sm:grid-cols-6 sm:gap-3 sm:mb-5">
+
+                        <div class="col-span-full sm:col-span-3 ">
+                            <label class="block text-sm font-medium text-gray-900 dark:text-white" for="title">
+                                Atividade
+                            </label>
+                            <select wire:model="extra_activities_id"
+                                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500">
+                                <option value="">Selecione...</option>
+                                @foreach ($activities as $item)
+                                    <option value="{{ $item->id }}">{{ $item->title }}</option>
+                                @endforeach
+                            </select>
+                            @error('extra_activities_id')
+                                <span class="error">{{ $message }}</span>
+                            @enderror
+                        </div>
+                        <div class="col-span-full sm:col-span-1">
+                            <label class="block text-sm font-medium text-gray-900 dark:text-white" for="title">
+                                Ganhar GIP
+                            </label>
+                            <x-layout.toggle-true-false id="gip"
+                                active="{{ $gip }}"></x-layout.toggle-true-false>
+                            @error('gip')
+                                <span class="error">{{ $message }}</span>
+                            @enderror
+                        </div>
+                    </div>
+                </div>
+            </form>
+
+        </x-slot>
+        <x-slot name="footer">
+            <div class="px-4 text-right">
+                <button type="submit" wire:click="addActivity"
+                    class="text-white
+                                bg-green-700 hover:bg-green-800
+                                focus:ring-4 focus:outline-none focus:ring-green-300
+                                font-medium rounded-lg text-sm px-5 py-2.5
+                                text-center dark:bg-green-600 dark:hover:bg-green-700
+                                dark:focus:ring-green-800">
+                    Salvar e sair
+                </button>
+            </div>
+        </x-slot>
+    </x-dialog-modal>
 </div>

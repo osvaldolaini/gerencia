24617ef5fc3424c2de9c8727bfd9edb027387649
Osvaldo Laini <osvaldolaini@hotmail.com>
2025-07-08 13:39:35 -0300

    Versão Santos Dumont 1.7.0 – 2025-06-26

--- app/Livewire/Students/StudentContact.php
+++ app/Livewire/Students/StudentContact.php
@@ -41,6 +41,7 @@ class StudentContact extends Component
 
     public function removeRow($id)
     {
+        dd($id);
         $contact = StudentContacts::find($id);
         $contact->active = 0;
         $contact->save();

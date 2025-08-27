93bdfc917e7e91f2739efd9cd709e63da61ff843
Adão Gomes De Oliveira Júnior <524247@casaraocmpa.intranet.br>
2025-08-27 14:45:17 -0300

    Versão Santos Dumont 1.7.1 – 2025-08-27

--- resources/views/livewire/discipline/fault-disciplines/pdfs/status-pdf.blade.php
+++ resources/views/livewire/discipline/fault-disciplines/pdfs/status-pdf.blade.php
@@ -74,7 +74,7 @@
    
     // Agrupa por 'decision' e conta
     $penaltiesCount = $data->groupBy('decision')->map->count();
-    $load = $data->where('decision','')->map->count();
+    $load = $data->where('decision','')->count();
 
     // Monta resumo baseado no Enum (mantendo ordem)
     $penaltySummary = [];

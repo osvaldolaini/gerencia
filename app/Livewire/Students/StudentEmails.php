c34313b8a7ba4017c7a8368b45d1552055cf0cc4
Adão Gomes De Oliveira Júnior <524247@casaraocmpa.intranet.br>
2025-09-05 11:59:14 -0300

    Versão Santos Dumont 1.7.2 – 2025-09-03

--- app/Livewire/Students/StudentEmails.php
+++ app/Livewire/Students/StudentEmails.php
@@ -55,7 +55,7 @@ class StudentEmails extends Component
 
         if ($this->contacts->count() > 0) {
             foreach ($this->contacts as $contact) {
- $totalEmails++;
+                $totalEmails++;
                 $this->attachment = trim('ficha_individual_' . $this->student->number . '_' . $this->slug($this->student->nick) . '_' . Str::uuid() . '.pdf');
 
                 if ($contact->type == 'email') {

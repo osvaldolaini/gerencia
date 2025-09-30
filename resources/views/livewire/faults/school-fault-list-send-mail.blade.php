0bd464c5f53a103f2675f4ab0ae9c6af756d023c
Adão Gomes De Oliveira Júnior <524247@casaraocmpa.intranet.br>
2025-09-30 11:06:19 -0300

    Versão Santos Dumont 1.7.1 – 2025-09-30

--- resources/views/livewire/faults/school-fault-list-send-mail.blade.php
+++ resources/views/livewire/faults/school-fault-list-send-mail.blade.php
@@ -26,84 +26,70 @@
         <!-- Lista de itens arrastáveis -->
         <div>
             @foreach ($dataTable as $item)
-                <div class="mb-10 rounded-md cursor-pointer">
-                    <h2 id="w-full text-center items-center">
-                        <div type="button"
-                            class="items-center justify-between w-full p-5 font-medium text-left text-gray-500 border border-gray-200 dark:bg-gray-900 rounded-xl focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-800 dark:border-gray-700 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-800">
-                            <div class="grid grid-cols-8 gap-2 mx-2 ">
-                                <div class="pl-2 col-span-full sm:col-span-1">
-                                    @if ($item->id)
-                                        @if ($item->path)
-                                            <img src="{{ url('storage/student/' . $item->id . '/' . $item->path) }}"
-                                                class="mx-auto rounded-md">
+                @if ($item->total_faults_percent > 6.0)
+                    <div class="mb-10 rounded-md cursor-pointer">
+                        <h2 id="w-full text-center items-center">
+                            <div type="button"
+                                class="items-center justify-between w-full p-5 font-medium text-left text-gray-500 border border-gray-200 dark:bg-gray-900 rounded-xl focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-800 dark:border-gray-700 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-800">
+                                <div class="grid grid-cols-8 gap-2 mx-2 ">
+                                    <div class="pl-2 col-span-full sm:col-span-1">
+                                        @if ($item->id)
+                                            @if ($item->path)
+                                                <img src="{{ url('storage/student/' . $item->id . '/' . $item->path) }}"
+                                                    class="mx-auto rounded-md">
+                                            @else
+                                                <x-application-logo width="h-12"></x-application-logo>
+                                            @endif
                                         @else
                                             <x-application-logo width="h-12"></x-application-logo>
                                         @endif
-                                    @else
-                                        <x-application-logo width="h-12"></x-application-logo>
-                                    @endif
-                                </div>
-                                <div class="col-span-full sm:col-span-2">
-                                    <h1
-                                        class="text-3xl font-bold {{ $item->sex == 'F' ? 'text-red-500' : 'text-blue-500' }}">
-                                        Al. {{ $item->nick ?? $item?->oldSudents?->nick }}
-                                    </h1>
-                                    <div class="max-w-xs">
-                                        <p>
-                                            nº. {{ $item->number ?? $item?->oldSudents?->number }}
-                                        </p>
-                                        <p>
-                                            T. {{ $item?->al_class->title  ?? 'sem turma' }}
-                                        </p>
                                     </div>
-                                </div>
-                                <div class="space-y-2 col-span-full sm:col-span-3">
-                                   
-                                    <div class="flex-nowrap">
-                                        <span wire:click='justify({{ $item->id }},0)'
-                                            class="btn {{ $item->justified == 0 ? '' : 'btn-outline' }} btn-error btn-sm">
-                                            Não justificada
-                                        </span>
-                                        <span wire:click='justify({{ $item->id }},1)'
-                                            class="btn {{ $item->justified == 1 ? '' : 'btn-outline' }} btn-info btn-sm">
-                                            Justificada
-                                        </span>
-                                        <div class="p-0 tooltip tooltip-top"
-                                            data-tip="Art. 86, § 3º, III - o aluno que estiver representando o CM em atividade externa ou extra classe, terá a sua
-falta justificada (abono de falta). RICM 2024">
-                                            <span wire:click='justify({{ $item->id }},2)'
-                                                class="btn {{ $item->justified == 2 ? '' : 'btn-outline' }} btn-success btn-sm">
-                                                Abonada
-                                            </span>
+                                    <div class="col-span-full sm:col-span-2">
+                                        <h1
+                                            class="text-3xl font-bold {{ $item->sex == 'F' ? 'text-red-500' : 'text-blue-500' }}">
+                                            Al. {{ $item->nick ?? $item?->oldSudents?->nick }}
+                                        </h1>
+                                        <div class="max-w-xs">
+                                            <p>
+                                                nº. {{ $item->number ?? $item?->oldSudents?->number }}
+                                            </p>
+                                            <p>
+                                                T. {{ $item?->al_class->title  ?? 'sem turma' }}
+                                            </p>
                                         </div>
                                     </div>
+                                    <div class="space-y-2 col-span-full sm:col-span-3">
+                        
 
-                                </div>
-                                <div class="col-span-full sm:col-span-2">
-                                    <div class="justify-start block space-x-2 space-y-2 font-medium duration-200 ">
-
-                                        @if (in_array('update', auth()->user()->jsonActivities))
-                                            <button wire:click='showUpdate({{ $item->id }})'
-                                                class="btn btn-outline dark:btn-accent btn-sm">
-                                                Editar
-                                                <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6 " fill="none"
-                                                    viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-                                                    <path stroke-linecap="round" stroke-linejoin="round"
-                                                        d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z">
-                                                    </path>
-                                                </svg>
-                                            </button>
-                                        @endif
-
-                                        
+                                    <span class="badge badge-{{ $item->total_faults_color }}">
+                                        Faltas: {{ $item->total_faults }}
+                                        ({{ number_format($item->total_faults_percent, 2, ',', '') }}%)
+                                    </span>
 
 
+                                    </div>
+                                    <div class="col-span-full sm:col-span-2">
+                                        <div class="justify-start block space-x-2 space-y-2 font-medium duration-200 ">
+
+                                            @if (in_array('update', auth()->user()->jsonActivities))
+                                                <button wire:click='showUpdate({{ $item->id }})'
+                                                    class="btn btn-outline dark:btn-accent btn-sm">
+                                                    Editar
+                                                    <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6 " fill="none"
+                                                        viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                                                        <path stroke-linecap="round" stroke-linejoin="round"
+                                                            d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z">
+                                                        </path>
+                                                    </svg>
+                                                </button>
+                                            @endif
+                                        </div>
                                     </div>
                                 </div>
                             </div>
-                        </div>
-                    </h2>
-                </div>
+                        </h2>
+                    </div>
+                @endif
             @endforeach
             <div class="items-center justify-between py-4" wire:ignore>
                 {{ $dataTable->links() }}

41cfa7bbe6da95df7654974c60e2a5908bf73f40
Osvaldo Laini <osvaldolaini@hotmail.com>
2025-10-02 15:06:04 -0300

    Versão Santos Dumont 1.7.1 – 2025-09-30

--- resources/views/livewire/discipline/fault-disciplines/fault-discipline-note.blade.php
+++ resources/views/livewire/discipline/fault-disciplines/fault-discipline-note.blade.php
@@ -1,6 +1,6 @@
 <div>
     <div class="flex items-center justify-center w-full mb-5 space-x-1">
-        <span wire:click="print()" class="btn btn-info">
+        <span wire:click="printNote()" class="btn btn-info">
             Imprimir Nota
             <svg class="w-6 h-6 mr-0 lg:mr-2" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <path fill-rule="evenodd" clip-rule="evenodd"

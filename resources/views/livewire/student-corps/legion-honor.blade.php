a386afbd794f66d794bb886b45ffaa5ea0c83f38
Osvaldo Laini <osvaldolaini@hotmail.com>
2025-11-06 19:03:01 -0300

    Versão Santos Dumont 1.7.3 – 2025-11-06

--- resources/views/livewire/student-corps/legion-honor.blade.php
+++ resources/views/livewire/student-corps/legion-honor.blade.php
@@ -189,10 +189,13 @@
             Excluir Legionario
         </x-slot>
         <x-slot name="content">
-            <h2 class="h2">Deseja realmente excluir
-                {{ $legionary?->student->sex == 'F' ? 'a legionária' : 'o legionário' }}
-                {{ $legionary?->student?->nick }} ({{ $legionary?->student?->number }})</h2>
-            <p>Não será possível reverter esta ação!</p>
+            @if ($legionary)
+                <h2 class="h2">Deseja realmente excluir
+                    {{ $legionary?->student->sex == 'F' ? 'a legionária' : 'o legionário' }}
+                    {{ $legionary?->student?->nick }} ({{ $legionary?->student?->number }})</h2>
+                <p>Não será possível reverter esta ação!</p>
+            @endif
+
         </x-slot>
 
         <x-slot name="footer">
@@ -211,95 +214,100 @@
             Excluir Legionario
         </x-slot>
         <x-slot name="content">
-            <h2 class="h2">Deseja realmente remover
-                {{ $legionary?->student->sex == 'F' ? 'a legionária' : 'o legionário' }}
-                {{ $legionary?->student?->nick }} ({{ $legionary?->student?->number }})</h2>
-            <p>Não será possível reverter esta ação!</p>
-            <form>
-                <x-layout.tabs>
-                    <x-slot name="nav">
-                        <x-slot name="svg">
-                            <svg class="w-5 h-5 transition duration-75 shrink-0 text-primary-600 dark:text-primary-400"
-                                fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
-                                <path stroke-linecap="round" stroke-linejoin="round"
-                                    d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
-                            </svg>
+            @if ($legionary)
+                <h2 class="h2">Deseja realmente remover
+                    {{ $legionary?->student->sex == 'F' ? 'a legionária' : 'o legionário' }}
+                    {{ $legionary?->student?->nick }} ({{ $legionary?->student?->number }})</h2>
+                <p>Não será possível reverter esta ação!</p>
+                <form>
+                    <x-layout.tabs>
+                        <x-slot name="nav">
+                            <x-slot name="svg">
+                                <svg class="w-5 h-5 transition duration-75 shrink-0 text-primary-600 dark:text-primary-400"
+                                    fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
+                                    <path stroke-linecap="round" stroke-linejoin="round"
+                                        d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
+                                </svg>
+                            </x-slot>
+                            <x-slot name="title">Legião de honra</x-slot>
+
                         </x-slot>
-                        <x-slot name="title">Legião de honra</x-slot>
+                        <x-slot name="content">
+                            <div id="tab1" x-show="activeTab === '#tab1'" class="block">
 
-                    </x-slot>
-                    <x-slot name="content">
-                        <div id="tab1" x-show="activeTab === '#tab1'" class="block">
+                                <div class="grid grid-cols-2 gap-2 mb-1 sm:grid-cols-6 sm:gap-3 sm:mb-5">
+                                    <div class="col-span-full ">
 
-                            <div class="grid grid-cols-2 gap-2 mb-1 sm:grid-cols-6 sm:gap-3 sm:mb-5">
-                                <div class="col-span-full ">
+                                        @if ($legionary)
+                                            <div role="alert" class="w-full col-span-1 shadow-xl alert ">
+                                                <figure>
+                                                    <img src="{{ url('storage/student/' . $legionary?->student->id . '/' . $legionary?->student->code_image . '_list.png') }}"
+                                                        class="mx-auto rounded ">
+                                                </figure>
+                                                <div>
+                                                    <h3 class="font-bold">Al. {{ $legionary?->student->nick }}</h3>
+                                                    <div class="text-xs">T. {{ $legionary?->student->class }}</div>
+                                                </div>
+                                            </div>
+                                            <div
+                                                class="grid grid-cols-4 col-span-1 gap-2 mb-1 sm:grid-cols-4 sm:gap-3 sm:mb-5">
+                                                <div class="col-span-full sm:col-span-1">
+                                                    <label
+                                                        class="block text-sm font-medium text-gray-900 dark:text-white"
+                                                        for="title">
+                                                        *Nota p/Bol Nr</label>
+                                                    <input type="number" wire:model="off_supplement_number"
+                                                        class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500">
+                                                    @error('off_supplement_number')
+                                                        <span class="error">{{ $message }}</span>
+                                                    @enderror
+                                                </div>
+                                                <div class="col-span-full sm:col-span-1">
+                                                    <label
+                                                        class="block text-sm font-medium text-gray-900 dark:text-white"
+                                                        for="title">
+                                                        *BI Nr</label>
+                                                    <input type="number" wire:model="off_bi_number"
+                                                        class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500">
+                                                    @error('off_bi_number')
+                                                        <span class="error">{{ $message }}</span>
+                                                    @enderror
+                                                </div>
+                                                <div class="col-span-full sm:col-span-2">
+                                                    <label
+                                                        class="block text-sm font-medium text-gray-900 dark:text-white"
+                                                        for="title">
+                                                        *Data publicação</label>
+                                                    <input type="date" wire:model="off_bi_date"
+                                                        class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500">
+                                                    @error('off_bi_date')
+                                                        <span class="error">{{ $message }}</span>
+                                                    @enderror
+                                                </div>
 
-                                    @if ($legionary)
-                                        <div role="alert" class="w-full col-span-1 shadow-xl alert ">
-                                            <figure>
-                                                <img src="{{ url('storage/student/' . $legionary?->student->id . '/' . $legionary?->student->code_image . '_list.png') }}"
-                                                    class="mx-auto rounded ">
-                                            </figure>
-                                            <div>
-                                                <h3 class="font-bold">Al. {{ $legionary?->student->nick }}</h3>
-                                                <div class="text-xs">T. {{ $legionary?->student->class }}</div>
-                                            </div>
-                                        </div>
-                                        <div
-                                            class="grid grid-cols-4 col-span-1 gap-2 mb-1 sm:grid-cols-4 sm:gap-3 sm:mb-5">
-                                            <div class="col-span-full sm:col-span-1">
-                                                <label class="block text-sm font-medium text-gray-900 dark:text-white"
-                                                    for="title">
-                                                    *Nota p/Bol Nr</label>
-                                                <input type="number" wire:model="off_supplement_number"
-                                                    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500">
-                                                @error('off_supplement_number')
-                                                    <span class="error">{{ $message }}</span>
-                                                @enderror
-                                            </div>
-                                            <div class="col-span-full sm:col-span-1">
-                                                <label class="block text-sm font-medium text-gray-900 dark:text-white"
-                                                    for="title">
-                                                    *BI Nr</label>
-                                                <input type="number" wire:model="off_bi_number"
-                                                    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500">
-                                                @error('off_bi_number')
-                                                    <span class="error">{{ $message }}</span>
-                                                @enderror
-                                            </div>
-                                            <div class="col-span-full sm:col-span-2">
-                                                <label class="block text-sm font-medium text-gray-900 dark:text-white"
-                                                    for="title">
-                                                    *Data publicação</label>
-                                                <input type="date" wire:model="off_bi_date"
-                                                    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500">
-                                                @error('off_bi_date')
-                                                    <span class="error">{{ $message }}</span>
-                                                @enderror
-                                            </div>
 
-
-                                            <div class="col-span-full sm:col-span-full ">
-                                                <label class="block text-sm font-medium text-gray-900 dark:text-white"
-                                                    for="title">
-                                                    Relato
-                                                </label>
-                                                <textarea wire:model="off_bi_text" rows="10"
-                                                    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"></textarea>
-                                                @error('text')
-                                                    <span class="error">{{ $message }}</span>
-                                                @enderror
+                                                <div class="col-span-full sm:col-span-full ">
+                                                    <label
+                                                        class="block text-sm font-medium text-gray-900 dark:text-white"
+                                                        for="title">
+                                                        Relato
+                                                    </label>
+                                                    <textarea wire:model="off_bi_text" rows="10"
+                                                        class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"></textarea>
+                                                    @error('text')
+                                                        <span class="error">{{ $message }}</span>
+                                                    @enderror
+                                                </div>
                                             </div>
-                                        </div>
-                                    @endif
+                                        @endif
+                                    </div>
                                 </div>
                             </div>
-                        </div>
 
-                    </x-slot>
-                </x-layout.tabs>
-            </form>
-
+                        </x-slot>
+                    </x-layout.tabs>
+                </form>
+            @endif
         </x-slot>
 
         <x-slot name="footer">

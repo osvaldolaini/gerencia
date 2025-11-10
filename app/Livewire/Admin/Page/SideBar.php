d1ba5c6ed517fb24b75361b4b279d3479b9d84ca
Osvaldo Laini <osvaldolaini@hotmail.com>
2025-11-10 13:30:48 -0300

    Versão Santos Dumont 1.8.0 – 2025-11-06

--- app/Livewire/Admin/Page/SideBar.php
+++ app/Livewire/Admin/Page/SideBar.php
@@ -13,7 +13,7 @@ class SideBar extends Component
     public function render()
     {
         $this->fo = FactObserved::where('active', 1)->where('fact_type', 'negativo')->where('sincomil_date', null)->where('fafd_id', null)->get()->count();
-        // dd($this->fo);
+        dd($this->fo);
         return view('livewire.admin.page.side-bar');
     }
 }

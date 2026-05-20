8356a47911eb06e90a80ecfb3164c820432dda5b
Osvaldo Laini <osvaldolaini@hotmail.com>
2026-05-20 17:38:20 -0300

    Versão Santos Dumont 1.8.6 – 2026-04-14

--- app/Mail/StudentRecordNew.php
+++ app/Mail/StudentRecordNew.php
@@ -10,17 +10,13 @@ use Illuminate\Mail\Mailables\Address;
 use Illuminate\Mail\Mailables\Attachment;
 use Illuminate\Mail\Mailables\Envelope;
 use Illuminate\Queue\SerializesModels;
-use Illuminate\Support\Facades\Config;
-use Illuminate\Support\Facades\Mail;
 
 class StudentRecordNew extends Mailable
 {
     use Queueable, SerializesModels;
-
     public $contact;
     public $attachment;
     public $company;
-
     /**
      * Create a new message instance.
      */
@@ -30,30 +26,6 @@ class StudentRecordNew extends Mailable
         $this->contact = $data['contact'];
         $this->attachment = $data['attachment'];
         $this->company = $data['company'];
-
-        /**
-         * CONFIGURA SMTP DINAMICAMENTE
-         * Dados vindos da empresa/setor
-         */
-        Config::set('mail.default', 'smtp');
-
-        Config::set('mail.mailers.smtp.transport', 'smtp');
-
-        Config::set('mail.mailers.smtp.host', $this->company->mail_host);
-        Config::set('mail.mailers.smtp.port', $this->company->mail_port);
-
-        Config::set('mail.mailers.smtp.username', $this->company->mail_username);
-        Config::set('mail.mailers.smtp.password', $this->company->mail_password);
-
-        Config::set('mail.mailers.smtp.encryption', $this->company->mail_encryption);
-
-        Config::set('mail.from.address', $this->company->mail_from_address);
-        Config::set('mail.from.name', $this->company->mail_from_name);
-
-        /**
-         * LIMPA CONEXÕES ANTIGAS
-         */
-        Mail::purge();
     }
 
     /**
@@ -62,25 +34,14 @@ class StudentRecordNew extends Mailable
     public function envelope(): Envelope
     {
         $config = Settings::find(1);
-
+        // dd($this->data['company']->email, $this->data['company']);
         return new Envelope(
-            from: new Address(
-                $this->company->mail_from_address,
-                $this->company->mail_from_name . ' - ' . $config->nick
-            ),
-
+            from: new Address($this->data['company']->email, $this->data['company']->nick . ' - ' . $config->nick),
             to: [
-                new Address(
-                    $this->contact->contact,
-                    $this->contact->parent
-                ),
+                new Address($this->data['contact']->contact, $this->data['contact']->parent),
             ],
-
             subject: 'Ficha individual',
-
-            tags: [
-                $config->nick
-            ],
+            tags: [$config->nick],
         );
     }
 
@@ -91,11 +52,8 @@ class StudentRecordNew extends Mailable
     {
         return new Content(
             view: 'livewire.students.student-record',
-
             with: [
                 'config' => Settings::find(1),
-                'company' => $this->company,
-                'contact' => $this->contact,
             ],
         );
     }
@@ -103,18 +61,15 @@ class StudentRecordNew extends Mailable
     /**
      * Get the attachments for the message.
      *
-     * @return array<int, Attachment>
+     * @return array<int, \Illuminate\Mail\Mailables\Attachment>
      */
     public function attachments(): array
     {
+        // dd(storage_path('app/pdf-tmp/' . $this->data['attachment']));
         return [
-            Attachment::fromPath(
-                storage_path(
-                    'app/public/pdf-tmp/' . $this->attachment
-                )
-            )
-                ->as($this->attachment)
-                ->withMime('application/pdf'),
+            Attachment::fromPath(url('storage/pdf-tmp/' . $this->data['attachment']))
+                ->as($this->data['attachment'])
+                ->withMime('application/pdf')
         ];
     }
 }
---
+++ app/Mail/StudentRecordNeww.php
@@ -0,0 +1,120 @@
+<?php
+
+namespace App\Mail;
+
+use App\Models\Admin\Settings\Settings;
+use Illuminate\Bus\Queueable;
+use Illuminate\Mail\Mailable;
+use Illuminate\Mail\Mailables\Content;
+use Illuminate\Mail\Mailables\Address;
+use Illuminate\Mail\Mailables\Attachment;
+use Illuminate\Mail\Mailables\Envelope;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\Config;
+use Illuminate\Support\Facades\Mail;
+
+class StudentRecordNew extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    public $contact;
+    public $attachment;
+    public $company;
+
+    /**
+     * Create a new message instance.
+     */
+    public function __construct(
+        public readonly array $data
+    ) {
+        $this->contact = $data['contact'];
+        $this->attachment = $data['attachment'];
+        $this->company = $data['company'];
+
+        /**
+         * CONFIGURA SMTP DINAMICAMENTE
+         * Dados vindos da empresa/setor
+         */
+        Config::set('mail.default', 'smtp');
+
+        Config::set('mail.mailers.smtp.transport', 'smtp');
+
+        Config::set('mail.mailers.smtp.host', $this->company->mail_host);
+        Config::set('mail.mailers.smtp.port', $this->company->mail_port);
+
+        Config::set('mail.mailers.smtp.username', $this->company->mail_username);
+        Config::set('mail.mailers.smtp.password', $this->company->mail_password);
+
+        Config::set('mail.mailers.smtp.encryption', $this->company->mail_encryption);
+
+        Config::set('mail.from.address', $this->company->mail_from_address);
+        Config::set('mail.from.name', $this->company->mail_from_name);
+
+        /**
+         * LIMPA CONEXÕES ANTIGAS
+         */
+        Mail::purge();
+    }
+
+    /**
+     * Get the message envelope.
+     */
+    public function envelope(): Envelope
+    {
+        $config = Settings::find(1);
+
+        return new Envelope(
+            from: new Address(
+                $this->company->mail_from_address,
+                $this->company->mail_from_name . ' - ' . $config->nick
+            ),
+
+            to: [
+                new Address(
+                    $this->contact->contact,
+                    $this->contact->parent
+                ),
+            ],
+
+            subject: 'Ficha individual',
+
+            tags: [
+                $config->nick
+            ],
+        );
+    }
+
+    /**
+     * Get the message content definition.
+     */
+    public function content(): Content
+    {
+        return new Content(
+            view: 'livewire.students.student-record',
+
+            with: [
+                'config' => Settings::find(1),
+                'company' => $this->company,
+                'contact' => $this->contact,
+            ],
+        );
+    }
+
+    /**
+     * Get the attachments for the message.
+     *
+     * @return array<int, Attachment>
+     */
+    public function attachments(): array
+    {
+        return [
+            Attachment::fromPath(
+                storage_path(
+                    'app/public/pdf-tmp/' . $this->attachment
+                )
+            )
+                ->as($this->attachment)
+                ->withMime('application/pdf'),
+        ];
+    }
+}
